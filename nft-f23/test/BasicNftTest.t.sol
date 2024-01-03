4bca66bd94c99db760cc2eba374b8c61c2fe957c
jennyg0 <jen2348@hotmail.com>
2024-01-02 22:19:03 -0500

    create nft contract and deploy script

---
+++ nft-f23/test/BasicNftTest.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
+
+contract BasicNftTest is Test {}
