9cd25442c0aff2b524adf52b1f1a4095dcc1a5db
Jen <60794961+jennyg0@users.noreply.github.com>
2024-08-01 15:20:10 -0700

    add scripts

---
+++ account-abstraction-f24/script/SendPackedUserOp.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {MinimalAccount} from "src/ethereum/MinimalAccount.sol";
+// import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+
+contract SendPackedUserOp is Script {}
