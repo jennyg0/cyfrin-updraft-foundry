f31ec6d31534b14dd76aaa4119c43ac007a38581
Jen <60794961+jennyg0@users.noreply.github.com>
2024-08-09 21:17:09 -0400

    transaction simulations

--- account-abstraction-f24/src/zksync/ZKMinimalAccount.sol
+++ account-abstraction-f24/src/zksync/ZKMinimalAccount.sol
@@ -1,46 +1,44 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
 import {IAccount} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
 import {Transaction} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
+import {SystemContractsCaller} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/SystemContractsCaller.sol";
+import {NONCE_HOLDER_SYSTEM_CONTRACT, BOOTLOADER_FORMAL_ADDRESS, DEPLOYER_SYSTEM_CONTRACT} from "lib/foundry-era-contracts/src/system-contracts/contracts/Constants.sol";
+import {INonceHolder} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/INonceHolder.sol";
 
 contract ZKMinimalAccount is IAccount {
     function validateTransaction(
         bytes32,
         /*_txHash*/ bytes32,
         /*_suggestedSignedHash*/ Transaction memory _transaction
-    ) external payable requireFromBootLoader returns (bytes4 magic) {
-        return _validateTransaction(_transaction);
+    ) external payable returns (bytes4 magic) {
+        SystemContractsCaller.systemCallWithPropagatedRevert(
+            (uint32(gasleft())),
+            address(NONCE_HOLDER_SYSTEM_CONTRACT),
+            0,
+            abi.encodeCall(
+                INonceHolder.incrementMinNonceIfEquals,
+                (_transaction.nonce)
+            )
+        );
     }
 
     function executeTransaction(
         bytes32,
         /*_txHash*/ bytes32,
         /*_suggestedSignedHash*/ Transaction memory _transaction
-    ) external payable requireFromBootLoaderOrOwner {
-        _executeTransaction(_transaction);
-    }
+    ) external payable {}
 
     function executeTransactionFromOutside(
         Transaction memory _transaction
-    ) external payable {
-        bytes4 magic = _validateTransaction(_transaction);
-        if (magic != ACCOUNT_VALIDATION_SUCCESS_MAGIC) {
-            revert ZkMinimalAccount__InvalidSignature();
-        }
-        _executeTransaction(_transaction);
-    }
+    ) external payable {}
 
     function payForTransaction(
         bytes32,
         /*_txHash*/ bytes32,
         /*_suggestedSignedHash*/ Transaction memory _transaction
-    ) external payable {
-        bool success = _transaction.payToTheBootloader();
-        if (!success) {
-            revert ZkMinimalAccount__FailedToPay();
-        }
-    }
+    ) external payable {}
 
     function prepareForPaymaster(
         bytes32 _txHash,
