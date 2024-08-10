c2ab72c99ad6a179d7dbde4373995e9189264a84
Jen <60794961+jennyg0@users.noreply.github.com>
2024-08-10 13:21:08 -0400

    validate transaction func

--- account-abstraction-f24/src/zksync/ZKMinimalAccount.sol
+++ account-abstraction-f24/src/zksync/ZKMinimalAccount.sol
@@ -1,18 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {IAccount} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
-import {Transaction} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
+import {IAccount, ACCOUNT_VALIDATION_SUCCESS_MAGIC} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
+import {Transaction, MemoryTransactionHelper} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
 import {SystemContractsCaller} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/SystemContractsCaller.sol";
 import {NONCE_HOLDER_SYSTEM_CONTRACT, BOOTLOADER_FORMAL_ADDRESS, DEPLOYER_SYSTEM_CONTRACT} from "lib/foundry-era-contracts/src/system-contracts/contracts/Constants.sol";
 import {INonceHolder} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/INonceHolder.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ZKMinimalAccount is IAccount {
+contract ZKMinimalAccount is IAccount, Ownable {
+    using MemoryTransactionHelper for Transaction;
+
+    error ZkMinimalAccount__NotEnoughBalance();
+    error ZkMinimalAccount__NotFromBootLoader();
+
+    modifier requireFromBootloader() {
+        if (msg.sender != BOOTLOADER_FORMAL_ADDRESS)
+            revert ZkMinimalAccount__NotFromBootLoader();
+        _;
+    }
+
+    constructor() Ownable(msg.sender) {}
+
     function validateTransaction(
         bytes32,
         /*_txHash*/ bytes32,
         /*_suggestedSignedHash*/ Transaction memory _transaction
-    ) external payable returns (bytes4 magic) {
+    ) external payable rrequireFromBootloadereturns (bytes4 magic) {
         SystemContractsCaller.systemCallWithPropagatedRevert(
             (uint32(gasleft())),
             address(NONCE_HOLDER_SYSTEM_CONTRACT),
@@ -22,6 +38,21 @@ contract ZKMinimalAccount is IAccount {
                 (_transaction.nonce)
             )
         );
+
+        uint256 totalRequiredBalance = _transaction.totalRequiredBalance();
+        if (totalRequiredBalance > address(this).balance) {
+            revert ZkMinimalAccount__NotEnoughBalance();
+        }
+
+        bytes32 txHash = _transaction.encodeHash();
+        address signer = ECDSA.recover(txHash, _transaction.signature);
+        bool isValidSigner = signer == owner();
+        if (isValidSigner) {
+            magic = ACCOUNT_VALIDATION_SUCCESS_MAGIC;
+        } else {
+            magic = bytes4(0);
+        }
+        return magic;
     }
 
     function executeTransaction(
