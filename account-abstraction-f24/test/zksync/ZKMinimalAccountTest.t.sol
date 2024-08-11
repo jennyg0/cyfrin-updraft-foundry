5dcaf3649c11f058b87f7e08ee69cf07dcc65997
Jen <60794961+jennyg0@users.noreply.github.com>
2024-08-11 09:30:09 -0400

    zksync tests

---
+++ account-abstraction-f24/test/zksync/ZKMinimalAccountTest.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {ZKMinimalAccount} from "src/zksync/ZKMinimalAccount.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+
+contract ZKMinimalAccountTest is Test {
+    ZKMinimalAccount minimalAccount;
+    ERC20Mock usdc;
+
+    uint256 constant AMOUNT = 1e18;
+
+    function setUp() public {
+        minimalAccount = new ZKMinimalAccount();
+        usdc = new ERC20Mock();
+    }
+
+    function testZKOwnerCanExecuteCommands() public {
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory functionData = abi.encodeWithSelector(
+            ERC20Mock.mint.selector,
+            address(minimalAccount),
+            AMOUNT
+        );
+    }
+}
